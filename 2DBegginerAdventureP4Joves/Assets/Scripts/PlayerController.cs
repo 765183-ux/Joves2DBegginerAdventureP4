fa5a1ea4e62950fd0a7fa9a49e9bb14e2da6fc64
765183-ux <765183@student.dusd.net>
2025-11-17 12:01:41 -0800

    Adding Scripts
    Adding Scripts to the player

--- 2DBegginerAdventureP4Joves/Assets/Scripts/PlayerController.cs
+++ 2DBegginerAdventureP4Joves/Assets/Scripts/PlayerController.cs
@@ -4,38 +4,27 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
+    public InputAction LeftAction;
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        MoveAction.Enable();
+   
+    
     }
 
     // Update is called once per frame
     void Update()
-    {
-        float horizontal = 0.0f;
-
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-
-
-        }
-        Debug.Log(horizontal);
-
-          float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
+    { 
+       {
+            Vector2 move = MoveAction.ReadValue<Vector2>();
 
             Vector2 position = transform.position;
-            position.x = position.x + 0.0f * horizontal;
-            position.y = position.y + 0.0f * vertical;
+          
             transform.position = position;
-        }
+       }
     }
 
 }
